d73e4407eb97a6528365c61caa59cdb667d6f4fe
abhishekchandra <shek.7.abhishek@gmail.com>
2024-06-18 14:58:12 +0000

    Introduction_To_React_Hooks_and_State_Hooks_Part_1

---
+++ src/components/GreetUser/index.js
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+import {
+  MainContainer,
+  NameInput,
+  MsgContent,
+  NameText,
+} from './styledComponents'
+
+const GreetUser = () => {
+  const [name, setName] = useState('')
+  console.log(name)
+  console.log('Component Rendered')
+
+  const onChangeInputName = event => setName(event.target.value)
+
+  return (
+    <MainContainer>
+      <NameInput
+        type="text"
+        placeholder="Your name"
+        onChange={onChangeInputName}
+        value={name}
+      />
+      <MsgContent>
+        {' '}
+        Hello <NameText>{name}</NameText>{' '}
+      </MsgContent>
+    </MainContainer>
+  )
+}
+
+export default GreetUser
